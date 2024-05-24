906b1dbbbcb0bed50167456fe2e079d01708d12f
xmNuc <xmnuc@o2.pl>
2024-05-24 12:05:02 +0200

    Added vertical list

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { Credits } from './components/Credits';
+// import { Credits } from './components/Credits';
+import VerticalList from './components/VerticaList';
 
 function App() {
   return (
     <>
-      <Credits />
+      {/* <Credits /> */}
+      <VerticalList />
     </>
   );
 }
